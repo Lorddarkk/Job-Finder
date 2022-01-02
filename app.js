d9cff6ce4172ad98eb52e2ed47f33fb66d74c72d
Luan <luannelmo1103@gmail.com>
2022-01-02 20:51:07 -0300

    Criando a rota para adicionar registros

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, function() {
     console.log(`O Express está rodando na porta ${PORT}`);
 });
+
+// body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Conexão do Banco de Dados
 db
@@ -22,3 +26,6 @@ db
 app.get('/', (req, res) => {
     res.send("Está funcionando :)");
 });
+
+// Jobs Routes
+app.use('/jobs', require('./routes/jobs'));
