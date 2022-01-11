dfc84fe8094dd6e36007010903f59361fe235dd7
Luan <luannelmo1103@gmail.com>
2022-01-10 22:03:50 -0300

    Criando view da job individual

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,10 +3,21 @@ const res = require('express/lib/response');
 const router = express.Router();
 const Job = require('../models/Job');
 
+// rota de teste
 router.get('/test', (req, res) => {
     res.send('Funcionou :)');
 })
 
+// detalhe da vaga -> view/1, view/2
+router.get('/view/:id', (req, res) => Job.findOne({
+    where: { id: req.params.id }
+}).then(job => {
+    res.render('view', {
+        job
+    });
+}).catch(err => console.log(err)));
+
+// form da rota de envio
 router.get('/add', (req, res) => {
     res.render('add');
 })
